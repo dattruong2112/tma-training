629d642eea9d1d5ccaaaf10b4f367db838a1daee
truongquocdanh0803 <truongquocdanh0803@gmail.com>
2024-08-08 14:50:15 +0700

    commit change statelessWidgetDemo1

--- training-widget/lib/statelessWidgetDemo1.dart
+++ training-widget/lib/statelessWidgetDemo1.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class MyAppBar extends StatelessWidget {
   const MyAppBar({required this.title, super.key});
@@ -9,8 +11,8 @@ class MyAppBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 60,
-      padding: EdgeInsets.symmetric(vertical: 8.0),
-      decoration: BoxDecoration(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      decoration: const BoxDecoration(
         color: Colors.blueAccent,
       ),
       child: Row(
@@ -41,8 +43,19 @@ class MyScaffold extends StatelessWidget {
             style: Theme.of(context).primaryTextTheme.titleLarge,
           ),
         ),
-        const Expanded(
-          child: Text('Product List', textAlign: TextAlign.center),
+          const Row(
+            children: [
+              Padding(padding: EdgeInsets.only(left: 350, top: 120)),
+              Text('Welcome to Lorem Ipsum', style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),)
+            ],
+            ),
+          const Expanded(
+            child: Center(
+              child: Text('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus tellus lectus, ornare accumsan mi id, volutpat convallis lectus. Donec consequat ipsum non lorem pharetra venenatis sit amet at erat. Quisque sit amet commodo elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Morbi sed commodo arcu, quis aliquam metus. Suspendisse quis tellus sed massa hendrerit convallis. Ut ipsum elit, volutpat a blandit vitae, finibus quis sem. Morbi blandit, nunc iaculis pulvinar hendrerit, eros ex lacinia lectus, vel facilisis eros sem eget quam. Integer fringilla, libero eget dictum vulputate, tortor eros dapibus arcu, sit amet venenatis sem nulla sed ligula. Vivamus eu felis eleifend lorem molestie luctus et id velit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae',
+               style: TextStyle(fontSize: 18,),
+                textAlign: TextAlign.center,
+                ),
+            ),
           ),
         ],
       ),
